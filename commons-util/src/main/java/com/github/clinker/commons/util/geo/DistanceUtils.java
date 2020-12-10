aff0a400aec0182cf1312e8e90093d9d444f476a
Chen Hua <clinker@163.com>
2020-12-10 14:35:55 +0800

    单位改成大写

--- commons-util/src/main/java/com/github/clinker/commons/util/geo/DistanceUtils.java
+++ commons-util/src/main/java/com/github/clinker/commons/util/geo/DistanceUtils.java
@@ -7,7 +7,7 @@ public class DistanceUtils {
 
 	/**
 	 * 根据距离长短加上不同的单位。有两个单位：米和千米。小于1000则表示为米；否则，表示为千米，十分位四舍五入到百分位。例如800->800米， 1001
-	 * ->1km，1500->1.5km，
+	 * ->1KM，1500->1.5KM。
 	 *
 	 * @param distance 距离，单位：米
 	 * @return 单位为千米或米的距离
@@ -15,7 +15,7 @@ public class DistanceUtils {
 	public static String smartDistance(final double distance) {
 		final boolean isKm = distance >= 1000;
 
-		final String unit = isKm ? "km" : "m";
+		final String unit = isKm ? "KM" : "M";
 		final double num = isKm ? distance / 1000.0 : distance;
 
 		String formatted = String.format("%.1f", num);
